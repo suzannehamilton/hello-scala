3fa1e2ec76662393bf69f5415fbc4e46185970ee
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2014-03-13 14:54:14 +0000

    Test that you can get the values from an Option using matching

--- src/test/scala/OptionSpec.scala
+++ src/test/scala/OptionSpec.scala
@@ -8,4 +8,13 @@ class OptionSpec extends UnitSpec {
   it should "return None if the key is not present" in {
     assert((capitals get "Not a country") == None)
   }
+
+  "an optional value" can "be taken apart with case statements" in {
+    assert(show(capitals get "France") == "Paris")
+  }
+
+  def show(x: Option[String]) = x match {
+    case Some(s) => s
+    case None => "?"
+  }
 }
