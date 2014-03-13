45c6c30322dc4238ad5b42aab4d9b2033c7baaac
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2014-03-13 18:13:20 +0000

    Test usage of 'map' on the Option case class

--- src/test/scala/OptionSpec.scala
+++ src/test/scala/OptionSpec.scala
@@ -1,3 +1,7 @@
+/**
+ * Helpful guide to the Option class:
+ * http://danielwestheide.com/blog/2012/12/19/the-neophytes-guide-to-scala-part-5-the-option-type.html
+ */
 class OptionSpec extends UnitSpec {
   val capitals = Map("France" -> "Paris", "Japan" -> "Tokyo")
 
@@ -21,6 +25,19 @@ class OptionSpec extends UnitSpec {
 
   it should "be None if created from null using the factory method" in {
     assert(Option(null) == None)
+  }
+
+  it can "be mapped to another Option value" in {
+    val firstOption = Some("foo")
+    val secondOption = firstOption.map(_.charAt(0))
+
+    assert(secondOption == Some('f'))
+  }
+
+  it should "be mapped to None if the original Option is None" in {
+    val firstOption: Option[String] = None
+
+    assert(firstOption.map(_.charAt(0)) == None)
   }
 
   def show(x: Option[String]) = x match {
