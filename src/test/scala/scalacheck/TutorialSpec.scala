1f1681231e300c16c39e0a85c6c31538b284c4b1
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-04-06 17:10:05 +0100

    Get basic StringSpecification working in ScalaCheck

--- src/test/scala/scalacheck/TutorialSpec.scala
+++ src/test/scala/scalacheck/TutorialSpec.scala
@@ -1,19 +1,17 @@
-import org.scalacheck.Prop
+import org.scalacheck._
 
 /**
  * Following the ScalaCheck user guide at https://github.com/rickynils/scalacheck/wiki/User-Guide
  */
-class TutorialSpec extends UnitSpec {
-  "Scalacheck" can "test that lists can be concatenated" in {
-    val propConcatLists = Prop.forAll { (l1: List[Int], l2: List[Int]) =>
+class TutorialSpec extends Properties("List") {
+  import Prop.forAll
+
+  property("concatenation") = forAll { (l1: List[Int], l2: List[Int]) =>
       l1.size + l2.size == (l1 ::: l2).size }
 
-    propConcatLists.check
-  }
+  // it should "reject the square root assertion" in {
+  //   val propSqrt = Prop.forAll { (n: Int) => scala.math.sqrt(n*n) == n }
 
-  it should "reject the square root assertion" in {
-    val propSqrt = Prop.forAll { (n: Int) => scala.math.sqrt(n*n) == n }
-
-    propSqrt.check
-  }
+  //   propSqrt.check
+  // }
 }
