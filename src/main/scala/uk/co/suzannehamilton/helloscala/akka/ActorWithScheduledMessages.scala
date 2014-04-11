357993d1879f9b4a00cf97f23fd72632e77306d3
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2014-04-11 14:56:50 +0100

    Set up a basic test of sending messages to an actor

--- src/main/scala/uk/co/suzannehamilton/helloscala/akka/ActorWithScheduledMessages.scala
+++ src/main/scala/uk/co/suzannehamilton/helloscala/akka/ActorWithScheduledMessages.scala
@@ -1,14 +1,16 @@
 package uk.co.suzannehamilton.helloscala.akka
 
 import akka.actor.{PoisonPill, ActorLogging, Actor}
-import uk.co.suzannehamilton.helloscala.akka.ActorWithScheduledMessages.Expire
+import uk.co.suzannehamilton.helloscala.akka.ActorWithScheduledMessages._
 
 class ActorWithScheduledMessages extends Actor with ActorLogging {
   override def receive = {
-    case Expire => self ! PoisonPill
+    case Expire() => self ! PoisonPill
+    case Ping() => sender ! "pong"
   }
 }
 
 object ActorWithScheduledMessages {
   case class Expire()
+  case class Ping()
 }
--- src/test/scala/uk/co/suzannehamilton/helloscala/akka/TimerSpec.scala
+++ src/test/scala/uk/co/suzannehamilton/helloscala/akka/TimerSpec.scala
@@ -2,15 +2,29 @@ package uk.co.suzannehamilton.helloscala.akka
 
 import uk.co.suzannehamilton.helloscala.specs2.Specification
 import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.testkit.{TestActorRef, ImplicitSender, TestKit}
+import uk.co.suzannehamilton.helloscala.akka.ActorWithScheduledMessages.{Ping, Expire}
 
 class TimerSpec extends Specification {
   abstract class Scope
     extends TestKit(ActorSystem())
     with org.specs2.specification.Scope
-    with ImplicitSender
+    with ImplicitSender {
 
-  "A custom message be scheduled" in new Scope {
-    true mustEqual true
+    val actor = TestActorRef[ActorWithScheduledMessages]
+  }
+
+  "Actor responds to pings" in new Scope {
+    actor ! Ping()
+
+    expectMsg("pong")
+  }
+
+  "Actor can be told to expire" in new Scope {
+    actor ! Expire()
+
+    actor ! Ping()
+
+    expectNoMsg()
   }
 }
