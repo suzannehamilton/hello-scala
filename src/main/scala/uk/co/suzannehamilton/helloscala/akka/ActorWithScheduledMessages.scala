443e58946d48ddf80922f58d8460c6027bc09570
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2014-04-11 15:19:05 +0100

    Test that an actor can be killed by scheduling a custom message which sends a poison pill OR by scheduling the poison pill directly

--- src/main/scala/uk/co/suzannehamilton/helloscala/akka/ActorWithScheduledMessages.scala
+++ src/main/scala/uk/co/suzannehamilton/helloscala/akka/ActorWithScheduledMessages.scala
@@ -2,15 +2,22 @@ package uk.co.suzannehamilton.helloscala.akka
 
 import akka.actor.{PoisonPill, ActorLogging, Actor}
 import uk.co.suzannehamilton.helloscala.akka.ActorWithScheduledMessages._
+import scala.concurrent.duration.FiniteDuration
 
 class ActorWithScheduledMessages extends Actor with ActorLogging {
+  import context._
+
   override def receive = {
     case Expire() => self ! PoisonPill
     case Ping() => sender ! "pong"
+    case ScheduleExpiry(delay) => context.system.scheduler.scheduleOnce(delay, self, Expire())
+    case SchedulePoisonPill(delay) => context.system.scheduler.scheduleOnce(delay, self, PoisonPill)
   }
 }
 
 object ActorWithScheduledMessages {
   case class Expire()
   case class Ping()
+  case class ScheduleExpiry(delay: FiniteDuration)
+  case class SchedulePoisonPill(delay: FiniteDuration)
 }
--- src/test/scala/uk/co/suzannehamilton/helloscala/akka/TimerSpec.scala
+++ src/test/scala/uk/co/suzannehamilton/helloscala/akka/TimerSpec.scala
@@ -1,9 +1,10 @@
 package uk.co.suzannehamilton.helloscala.akka
 
+import scala.concurrent.duration._
 import uk.co.suzannehamilton.helloscala.specs2.Specification
 import akka.actor.ActorSystem
 import akka.testkit.{TestActorRef, ImplicitSender, TestKit}
-import uk.co.suzannehamilton.helloscala.akka.ActorWithScheduledMessages.{Ping, Expire}
+import uk.co.suzannehamilton.helloscala.akka.ActorWithScheduledMessages._
 
 class TimerSpec extends Specification {
   abstract class Scope
@@ -22,6 +23,36 @@ class TimerSpec extends Specification {
 
   "Actor can be told to expire" in new Scope {
     actor ! Expire()
+
+    actor ! Ping()
+
+    expectNoMsg()
+  }
+
+  "Scheduling expiry should kill the actor after a delay" in new Scope {
+    actor ! ScheduleExpiry(new FiniteDuration(50, MILLISECONDS))
+
+    Thread.sleep(10)
+
+    actor ! Ping()
+    expectMsg("pong")
+
+    Thread.sleep(100)
+
+    actor ! Ping()
+
+    expectNoMsg()
+  }
+
+  "Scheduling poison pill should kill the actor after a delay" in new Scope {
+    actor ! SchedulePoisonPill(new FiniteDuration(50, MILLISECONDS))
+
+    Thread.sleep(10)
+
+    actor ! Ping()
+    expectMsg("pong")
+
+    Thread.sleep(100)
 
     actor ! Ping()
 
