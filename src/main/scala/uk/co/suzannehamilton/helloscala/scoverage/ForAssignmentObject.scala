f13556073434c1b33d6ed3af66254dca9583ec04
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2014-04-16 17:49:15 +0100

    Simplify further, to remove the string->long conversion

--- src/main/scala/uk/co/suzannehamilton/helloscala/scoverage/ForAssignmentObject.scala
+++ src/main/scala/uk/co/suzannehamilton/helloscala/scoverage/ForAssignmentObject.scala
@@ -3,18 +3,16 @@ package uk.co.suzannehamilton.helloscala.scoverage
 import scala.util.{Failure, Try}
 
 object ForAssignmentObject {
-  def splitId(id: String): Try[(String, Long)] = {
+  def splitId(id: String): Try[(String, String)] = {
     for {
-      (prefix, number) <- parseId(id)
-    } yield (prefix, number)
+      (prefix, suffix) <- parseId(id)
+    } yield (prefix, suffix)
   }
 
-  private val idPattern = """^([a-z])(\d)$""".r
-
-  private def parseId(id: String): Try[(String, Long)] = {
-    idPattern.findFirstMatchIn(id) match {
+  private def parseId(id: String): Try[(String, String)] = {
+    """^([a-z])(\d)$""".r.findFirstMatchIn(id) match {
       case Some(x) =>
-        Try((x.group(1), x.group(2).toLong))
+        Try((x.group(1), x.group(2)))
       case None =>
         Failure(new IllegalArgumentException("Did not match pattern"))
     }
