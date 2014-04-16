3f054d5acbc476041bbd2fede9b5ab4302a33f16
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2014-04-16 18:08:23 +0100

    Verify that expanded version of for is marked as covered by scoverage

--- src/main/scala/uk/co/suzannehamilton/helloscala/scoverage/ForAssignmentObject.scala
+++ src/main/scala/uk/co/suzannehamilton/helloscala/scoverage/ForAssignmentObject.scala
@@ -3,18 +3,22 @@ package uk.co.suzannehamilton.helloscala.scoverage
 import scala.util.{Failure, Try}
 
 object ForAssignmentObject {
-  def splitId(id: String): Try[(String, String)] = {
+  def splitIdUsingFor(id: String): Try[(String, String)] = {
     for {
       (prefix, suffix) <- parseId(id)
     } yield (prefix, suffix)
   }
 
+  def splitIdUsingFlatMap(id: String): Try[(String, String)] = {
+    (parseId(id)).map {
+      case (prefix, suffix) => (prefix, suffix)
+    }
+  }
+
   private def parseId(id: String): Try[(String, String)] = {
     """^([a-z])(\d)$""".r.findFirstMatchIn(id) match {
-      case Some(x) =>
-        Try((x.group(1), x.group(2)))
-      case None =>
-        Failure(new IllegalArgumentException("Did not match pattern"))
+      case Some(x) => Try((x.group(1), x.group(2)))
+      case None => Failure(new IllegalArgumentException("Did not match pattern"))
     }
   }
 }
--- src/test/scala/uk/co/suzannehamilton/helloscala/scoverage/ForAssignmentObjectTest.scala
+++ src/test/scala/uk/co/suzannehamilton/helloscala/scoverage/ForAssignmentObjectTest.scala
@@ -4,12 +4,22 @@ import org.scalatest.{OneInstancePerTest, FlatSpec}
 
 class ForAssignmentObjectTest extends FlatSpec with OneInstancePerTest {
   "ForAssignmentObject" should "split a valid ID" in {
-    val result = ForAssignmentObject.splitId("a1")
+    val result = ForAssignmentObject.splitIdUsingFor("a1")
     assert(result.isSuccess)
   }
 
   it should "return an error if the ID is invalid" in {
-    val result = ForAssignmentObject.splitId("")
+    val result = ForAssignmentObject.splitIdUsingFor("")
+    assert(result.isFailure)
+  }
+
+  "Flatmap version" should "split a valid ID" in {
+    val result = ForAssignmentObject.splitIdUsingFlatMap("a1")
+    assert(result.isSuccess)
+  }
+
+  it should "return an error if the ID is invalid" in {
+    val result = ForAssignmentObject.splitIdUsingFlatMap("")
     assert(result.isFailure)
   }
 }
