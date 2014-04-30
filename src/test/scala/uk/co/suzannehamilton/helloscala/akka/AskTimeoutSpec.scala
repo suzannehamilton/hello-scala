4380af4271165479e82c5cdceef47ea2483d07f9
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2014-04-30 14:06:38 +0100

    Test that an actor's response can be queried with an ask

--- src/test/scala/uk/co/suzannehamilton/helloscala/akka/AskTimeoutSpec.scala
+++ src/test/scala/uk/co/suzannehamilton/helloscala/akka/AskTimeoutSpec.scala
@@ -1,15 +1,22 @@
 package uk.co.suzannehamilton.helloscala.akka
 
+import akka.pattern.ask
 import uk.co.suzannehamilton.helloscala.specs2.Specification
 import akka.testkit.{TestActorRef, ImplicitSender, TestKit}
-import akka.actor.ActorSystem
+import akka.actor.{Status, ActorSystem}
 import uk.co.suzannehamilton.helloscala.akka.ActorMessages._
+import akka.util.Timeout
+import scala.concurrent.duration.FiniteDuration
+import java.util.concurrent.TimeUnit
+import scala.util.Success
 
 class AskTimeoutSpec extends Specification {
   abstract class Scope
     extends TestKit(ActorSystem())
     with org.specs2.specification.Scope
     with ImplicitSender {
+
+    implicit val timeout = Timeout(new FiniteDuration(1, TimeUnit.SECONDS))
 
     val actor = TestActorRef[ActorWhichRespondsImmediately]
 
@@ -20,6 +27,12 @@ class AskTimeoutSpec extends Specification {
     "respond to pings" in new Scope {
       actor ! Ping
       expectMsg(Pong)
+    }
+
+    "respond to pings sent in an ask" in new Scope {
+      val response = actor ? Ping
+
+      response.value.get mustEqual Success(Pong)
     }
   }
 }
